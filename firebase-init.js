12fe170591e165486416d9ec29ee78e48c0045e5
hector24 <hectortigre119@gmail.com>
2025-11-23 20:47:52 -0500

    preguntas de Servicio Comunitario cambios de preguntas

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBU1oaDdq6qD4fTiLN41SAeQg6Kp06gDXk",
-    authDomain: "simulador-tics.firebaseapp.com",
-    projectId: "simulador-tics",
-    storageBucket: "simulador-tics.firebasestorage.app",
-    messagingSenderId: "501091859008",
-    appId: "1:501091859008:web:80e4596d2adcb5adbf7da5",
-    measurementId: "G-5LFLE4MBPH"
-  };
+  apiKey: "AIzaSyAs_LBouq2njfy0cJHJjuiUfASC3RqVKkM",
+  authDomain: "practicas-comunitario.firebaseapp.com",
+  projectId: "practicas-comunitario",
+  storageBucket: "practicas-comunitario.firebasestorage.app",
+  messagingSenderId: "874017448238",
+  appId: "1:874017448238:web:03928054e98441838d1abf",
+  measurementId: "G-XF0GC0DZY4"
+};
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
